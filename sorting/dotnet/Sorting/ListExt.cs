171be3c3bb67a97f0d53d297c0ada851fe684c04
Wouter Groeneveld <wouter.groeneveld@gmail.com>
2018-03-21 17:19:24 +0100

    listExt name

---
+++ sorting/dotnet/Sorting/ListExt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sorting
+{
+    public static class ListExt
+    {
+        public static string AsString(this List<int> list)
+        {
+            return "(" + string.Join(", ", list.Select(x => x.ToString())) + ")";
+        }
+    }
+}
